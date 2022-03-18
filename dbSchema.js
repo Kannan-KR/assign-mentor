257474b0691b86c786f30e02cfe3a0fd687a4ced
Kannan <55331758+Kannan-KR@users.noreply.github.com>
2022-03-18 20:14:55 +0530

    v1

--- dbSchema.js
+++ dbSchema.js
@@ -1,6 +1,7 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 let dbName = "assign-mentor";
-let dbUrl = process.env.MONGO_URL;
+let dbUrl =
+  "mongodb+srv://john2727:9ZLfpwV9iViIlfCA@cluster0.bobsr.mongodb.net/${dbName}";
 
 module.exports = { dbUrl, mongodb, MongoClient };
